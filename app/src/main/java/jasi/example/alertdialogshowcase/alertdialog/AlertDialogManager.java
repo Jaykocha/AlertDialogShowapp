ecefc2100a8de21b2057a9fee462a82f3b13497e
jannes <sieblerjannes@gmail.com>
2022-02-16 12:22:27 +0100

    removed call to isVisible()

--- app/src/main/java/jasi/example/alertdialogshowcase/alertdialog/AlertDialogManager.java
+++ app/src/main/java/jasi/example/alertdialogshowcase/alertdialog/AlertDialogManager.java
@@ -3,28 +3,34 @@ package jasi.example.alertdialogshowcase.alertdialog;
 import android.content.Context;
 
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStoreOwner;
 
 public class AlertDialogManager {
-    public static MyAlertDialogViewModel initializeViewModel(ViewModelStoreOwner owner){
+
+    /**
+     * @param owner if out of Activity usually it is enough to just call 'this' or 'ActivityName.this'.
+     *              if initialized in a fragment always call requireActivity() instead
+     * @return MyAlertDialogViewModel which holds alert dialogs options
+     * */
+    public static MyAlertDialogViewModel initializeViewModel(ViewModelStoreOwner owner) {
         return new ViewModelProvider(owner).get(MyAlertDialogViewModel.class);
     }
 
+    /**
+     * @param callback  usually this, or ActivityName.this. Has to be class which extends any FragmentActivity
+     * @param type      the AlertType which defines what kind of alert to show
+     * @param viewModel the MyAlertDialogViewModel instance of the activity of where this function is called
+     * @param context   If called inside of a fragment, put requireActivity()
+     */
     public static void showMyDialog(MyAlertDialog.AlertDialogInterface callback,
                                     AlertType type,
                                     MyAlertDialogViewModel viewModel,
                                     Context context
-                                    ){
+    ) {
         MyAlertDialog dialog = MyAlertDialog.newInstance(callback);
         viewModel.setOptions(AlertOptions.create(type));
         viewModel.showDialog(); //important otherwise won't show
-        dialog.show(((FragmentActivity)context).getSupportFragmentManager(), type.toString());
-    }
-
-    public static boolean isVisible(Context context, AlertType type){
-        FragmentManager fragmentManager = ((FragmentActivity)context).getSupportFragmentManager();
-        return fragmentManager.findFragmentByTag(type.toString()) != null;
+        dialog.show(((FragmentActivity) context).getSupportFragmentManager(), type.toString());
     }
 }
